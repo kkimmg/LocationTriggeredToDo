16872184b471814687466a0366d4e34ff1ec02b1
owner <kkimmg@gmail.com>
2018-11-18 16:32:22 +0900

    init

---
+++ app/src/main/java/kkimmg/locationtriggeredtodo/LocationCheckService.java
@@ -0,0 +1,48 @@
+package kkimmg.locationtriggeredtodo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class LocationCheckService extends Service implements LocationListener {
+    private LocationManager manager;
+
+    private void initialize () {
+        manager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        manager.requestLocationUpdates(LocationManagger.GPS_PROVIDER, );
+    }
+
+
+    public LocationCheckService() {
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
